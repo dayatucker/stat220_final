1e613c604078da1839b4ec25ae26467bbdff6afe
dayatucker <dvtucker04@gmail.com>
2025-06-05 16:55:46 -0500

    cleanup

--- startfinal/lyrics_data_prep.R
+++ startfinal/lyrics_data_prep.R
@@ -1,7 +1,6 @@
-# Load genius library
-# Documentation: https://github.com/ewenme/geniusr
 
-library(geniusr)
+# load libraries
+library(geniusr) # Documentation: https://github.com/ewenme/geniusr
 library(dplyr)
 library(stringr)
 library(rvest)
@@ -11,12 +10,13 @@ library(tidyverse)
 
 combined_artists_tracks <- read_csv("stat220_final/startfinal/spotify_app/data/combined_artists_tracks_2018_2024.csv")
 combined_albums_tracks <- read_csv("stat220_final/startfinal/spotify_app/data/combined_albums_tracks_2018_2024.csv")
+
+# rename artist column to work with the function
 combined_albums_tracks <- combined_albums_tracks |>
   rename(artist_name = track_artists)
 
 # genius access token
 client_access_token <- paste(readLines("client_access_token.txt"), collapse = "")
-client_access_token <- readLines("client_access_token.txt")[1]
 genius_token(client_access_token)
 
 # helper function to clean lyrics
@@ -30,15 +30,14 @@ get_clean_lyrics <- function(url) {
     html_text(trim = TRUE) |>
     str_trim() |>
     keep(~ .x != "") |>
-    str_replace_all("[()]", "") |>  # remove just the parentheses, keep content
-    str_squish()     # clean up spacing
+    str_replace_all("[()]", "") |> 
+    str_squish()     
   
   lyrics_text <- paste(lines, collapse = " ")
   return(lyrics_text)
 }
 
-# function to pull lyrics from songs in a row containing artist_name and track_name
-# Main function to process the entire dataset
+# function to pull lyrics from songs in datasets
 get_lyrics <- function(tracks_data) {
   results <- vector("list", nrow(tracks_data))  # pre-allocate list
   
@@ -70,8 +69,6 @@ get_lyrics <- function(tracks_data) {
   
   bind_rows(results)
 }
-
-
 
 artist_tracks_with_lyrics <- get_lyrics(combined_artists_tracks)
 album_tracks_with_lyrics <- get_lyrics(combined_albums_tracks)
